788143f8984f3405e3f131c943b049b7a6e6db60
Philippe <Philippe@DESKTOP-3T3ABSL>
2016-04-26 22:50:15 +0200

    With Fake of Customer

---
+++ FakeCustomer/CustomerShould.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using NFluent;
+
+namespace FakeCustomer
+{
+    public class CustomerShould
+    {
+        [Test]
+        public void Return_Robin()
+        {
+            ICustomerRepository customerRepository = A.Fake<ICustomerRepository>();
+            A.CallTo(() => customerRepository.Get(1)).Returns("Robin");
+            var customer = new Customer(customerRepository);
+            var actual =customer.GetCustomerByID(1);
+            Check.That(actual).Equals("Robin");
+        }
+
+    }
+
+    public interface ICustomerRepository
+    {
+        string  Get(int Id);
+    }
+
+    public class Customer
+    {
+        private ICustomerRepository customerRepository;
+
+        public Customer(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public string GetCustomerByID(int customerId)
+        {
+            return customerRepository.Get(customerId);
+        }
+    }
+}
